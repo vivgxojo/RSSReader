6cc4b285d15a36a31f4cc3a55e17a2f51a47b9b6
Maryse Mongeau <vivgxojo@gmail.com>
2013-06-30 12:33:29 -0400

    fixed converter & javascript cleanup
    les fonctions utilitaires sont maintenant dans util.js

---
+++ FullFeedSuperRSSReader/FullFeedSuperRSSReader/util.js
@@ -0,0 +1,84 @@
+﻿//retourne une string à partir dUn dom xml
+function xmlToString(xmlData) {
+
+    var xmlString;
+    //IE
+    if (window.ActiveXObject) {
+        xmlString = xmlData.xml;
+    }
+    // code for Mozilla, Firefox, Opera, etc.
+    else {
+        xmlString = (new XMLSerializer()).serializeToString(xmlData);
+    }
+    return xmlString;
+}
+
+//retourne le dom d'une chaine xml
+function loadXMLString(txt) {
+    if (window.DOMParser) {
+        parser = new DOMParser();
+        xmlDoc = parser.parseFromString(txt, "text/xml");
+    }
+    else // Internet Explorer
+    {
+        xmlDoc = new ActiveXObject("Microsoft.XMLDOM");
+        xmlDoc.async = false;
+        xmlDoc.loadXML(txt);
+    }
+    return xmlDoc
+}
+
+//retourne le dom d'un fichier xml
+function loadXMLFile(file) {
+    if (window.ActiveXObject) {
+        xhttp = new ActiveXObject("Msxml2.XMLHTTP.3.0");
+    }
+    else {
+        xhttp = new XMLHttpRequest();
+    }
+    xhttp.open("GET", file, false);
+    xhttp.send("");
+    return xhttp.responseXML;
+}
+
+//retourne le résultat de la transformation xsl
+function transform(xml, xsl) {
+    if (window.ActiveXObject) {
+        ex = xml.transformNode(xsl);
+        return loadXMLString(ex);
+    }
+    // code for Mozilla, Firefox, Opera, etc.
+    else if (document.implementation && document.implementation.createDocument) {
+        xsltProcessor = new XSLTProcessor();
+        xsltProcessor.importStylesheet(xsl);
+        resultDocument = xsltProcessor.transformToFragment(xml, document);
+        return resultDocument;
+    }
+}
+
+function getCookie(c_name) {
+    var c_value = document.cookie;
+    var c_start = c_value.indexOf(" " + c_name + "=");
+    if (c_start == -1) {
+        c_start = c_value.indexOf(c_name + "=");
+    }
+    if (c_start == -1) {
+        c_value = null;
+    }
+    else {
+        c_start = c_value.indexOf("=", c_start) + 1;
+        var c_end = c_value.indexOf(";", c_start);
+        if (c_end == -1) {
+            c_end = c_value.length;
+        }
+        c_value = unescape(c_value.substring(c_start, c_end));
+    }
+    return c_value;
+}
+
+function setCookie(c_name, value, exdays) {
+    var exdate = new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var c_value = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toUTCString());
+    document.cookie = c_name + "=" + c_value;
+}
